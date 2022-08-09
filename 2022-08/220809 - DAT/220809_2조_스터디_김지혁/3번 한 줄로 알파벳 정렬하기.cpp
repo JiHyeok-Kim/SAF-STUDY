18898ffa497c4a5e5c652f8d35879eab3c7ed4c9
JiHyeok-Kim <109517003+JiHyeok-Kim@users.noreply.github.com>
2022-08-09 23:04:09 +0900

    220809

---
+++ 2022-08/220809 - DAT/220809_2조_스터디_김지혁/3번 한 줄로 알파벳 정렬하기.cpp
@@ -0,0 +1,37 @@
+﻿#include<iostream>
+using namespace std;
+
+char name[5][4] = {
+	"ABC",
+	"AGH",
+	"HIJ",
+	"KAB",
+	"ABC",
+};
+
+int main()
+{
+	int bucket[200] = { 0 };
+	int x, y;
+
+	for (y = 0; y < 5; y++)
+	{
+		for (x = 0; x < 3; x++)
+		{
+			bucket[name[y][x]]++;
+		}
+	}
+
+	for (y = 0; y < 200; y++)
+	{
+		if (bucket[y] > 0)
+		{
+			for (x = 0; x < bucket[y]; x++)
+			{
+				cout << (char)(y);
+			}
+		}
+	}
+
+	return 0;
+}
---
+++ 2022-08/220809 - DAT/220809_2조_스터디_김지혁/4번 초콜릿 선물.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+string str;
+int bucket[91];
+int main()
+{
+	cin >> str;
+	for (int i = 0; i < str.length(); i++) {
+		bucket[str[i]]++;
+	}
+	for (int i = 65; i < 91; i++) {
+		if (bucket[i] >= 1) { cout << char(i); }
+	}
+
+	return 0;
+}
