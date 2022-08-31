4649a0f515deaec2e2a8f5adbff76bd045c7f963
asd200df <109517068+asd200df@users.noreply.github.com>
2022-08-31 21:57:07 +0900

    0831

---
+++ 2022-08/220831 - SWEA 문제/220831_2조_스터디_이영도/DFS_1번_바이러스.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+vector<vector<int>> vec(101);
+int cp, n, cnt = 0;
+int used[101];
+
+void dfs(int now) {
+	cnt++;
+
+	for (int i = 0; i < vec[now].size(); i++) {
+		if (used[vec[now][i]] == 1) continue;
+		used[vec[now][i]] = 1;
+		// cnt++;
+		dfs(vec[now][i]);
+	}
+}
+
+int main() {
+	cin >> cp >> n;
+
+	for (int i = 0; i < n; i++) {
+		int a, b;
+		cin >> a >> b;
+		vec[a].push_back(b);
+		vec[b].push_back(a);
+	}
+
+	used[1] = 1;
+	dfs(1);
+
+	cout << cnt - 1 << endl;
+
+	return 0;
+}
