405933d2c0a0e40ae184a1d32fd5c5bb93b393cd
Zivallo <edgeak@naver.com>
2022-10-11 21:51:22 +0900

    20221011-SOOWAN-Update-5650

--- 2022-10/221011 - A형 대비 SWEA 문제 풀이/221011_스터디_조수완/5650_핀볼 게임.cpp
+++ 2022-10/221011 - A형 대비 SWEA 문제 풀이/221011_스터디_조수완/5650_핀볼 게임.cpp
@@ -1,96 +1,78 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <cstring>
 using namespace std;
-struct Node {
-	int y, x;
-};
-vector<Node> hole[20];
-int N, map[110][110], ans;
+int map[110][110], N, score, result;
 int drY[4] = { -1,0,1,0 };
 int drX[4] = { 0,1,0,-1 };
-int dir1[6][4] = { {0,0,0,0},{2,3,1,0},{1,3,0,2},{3,2,0,1},{2,0,3,1},{2,3,0,1} };
-int Wall(int dir) {
-	if (dir == 0) return 2;
-	if (dir == 1) return 3;
-	if (dir == 2) return 0;
-	if (dir == 3) return 1;
-	return dir;
+int block[6][4] = {
+	{0,0,0,0},
+	{2,3,1,0},
+	{1,3,0,2},
+	{3,2,0,1},
+	{2,0,3,1},
+	{2,3,0,1}
+};
+vector<pair<int, int>> wormhole[11];
+void init() {
+	for (int i = 6; i < 11; i++)wormhole[i].clear();
+	result = 0;
 }
-Node Hole(int num,int nowy, int nowx) {
-	for (int i = 0; i < hole[num].size(); i++) {
-		if (nowy != hole[num][i].y || nowx != hole[num][i].x) {
-			return { hole[num][i].y,hole[num][i].x };
-		}
+void input() {
+	cin >> N;
+	for (int y = 0; y < N; y++)for (int x = 0; x < N; x++) {
+		cin >> map[y][x];
+		if (map[y][x] > 5) wormhole[map[y][x]].push_back({ y, x });
 	}
-	return { nowy,nowx };
 }
-int dfs(int y, int x, int dir,int fy,int fx) {
-	int cnt = 0, nowy = y, nowx = x, ndir = dir;
-	int ny, nx;
-	while(1){
-		ny = nowy + drY[ndir];
-		nx = nowx + drX[ndir];
-		if (ny < 1 || nx < 1 || ny > N || nx > N) {
-			ndir = Wall(ndir);
-			nowy = ny;
-			nowx = nx;
-			cnt++;
+void simulation(int sy,int sx,int sdir) {
+	score = 0;
+	int y = sy, x = sx, dir = sdir;
+	while (1) {
+		y += drY[dir];
+		x += drX[dir];
+		if (y < 0 || x < 0 || y >= N || x >= N) {
+			dir = (dir + 2) % 4;
+			score++;
 			continue;
 		}
-		int val = map[ny][nx];
-		if ((ny == fy && nx == fx) || val == -1) return cnt;
-		if (0 < val && val < 6) {
-			ndir = dir1[val][ndir];
-			nowy = ny;
-			nowx = nx;
-			cnt++;
-			continue;
-		}
-		else if (val > 5) {
-			Node nCoor = Hole(val, ny, nx);
-			nowy = nCoor.y;
-			nowx = nCoor.x;
-			continue;
-		}
-		nowy = ny;
-		nowx = nx;
-	}
-	return cnt;
-}
-
-void sol() {
-	for (int y = 1; y <= N; y++) {
-		for (int x = 1; x <= N; x++) {
-			if (map[y][x] != 0)continue;
-			for (int i = 0; i < 4; i++) {
-				int cnt = dfs(y, x, i, y, x);
-				ans = max(ans, cnt);
+		if (map[y][x] == -1) break;
+		if (y == sy && x == sx) break;
+		if (map[y][x] == 0) continue;
+		if (map[y][x] > 5) {
+			int tar = map[y][x];
+			for (int i = 0; i < 2; i++) {
+				if (wormhole[tar][i].first == y && wormhole[tar][i].second == x) continue;
+				y = wormhole[tar][i].first;
+				x = wormhole[tar][i].second;
+				break;
 			}
+			continue;
+		}
+		if (map[y][x] > 0 && map[y][x] < 6) {
+			dir = block[map[y][x]][dir];
+			score++;
 		}
 	}
+	if (result < score) result = score;
 }
-
-int main() {
-	freopen_s(new FILE*, "input.txt", "r", stdin);
+void solve() {
 	int T;
 	cin >> T;
 	for (int t = 1; t <= T; t++) {
-		for (int i = 6; i < 13; i++) hole[i].clear();
-		memset(map, 0, sizeof(map));
-		ans = 0;
-		cin >> N;
-		for (int y = 1; y <= N; y++) {
-			for(int x = 1; x <= N; x++){
-				cin >> map[y][x];
-				if (map[y][x] >= 6) {
-					hole[map[y][x]].push_back({y,x});
-				}
-			}
+		init();
+		input();
+		for (int y = 0; y < N; y++)for (int x = 0; x < N; x++) {
+			if (map[y][x] != 0)continue;
+			for (int initDir = 0; initDir < 4; initDir++) simulation(y, x, initDir);
 		}
-		sol();
-		cout << "#" << t << " " << ans << "\n";
+		cout << "#" << t << " " << result << "\n";
 	}
+}
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//freopen_s(new FILE*, "input.txt", "r", stdin);
+	solve();
 	return 0;
 }
