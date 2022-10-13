6d089ecc85abce162def20c9e75a4f082811f5e4
HwangHyoSang <57944215+HHyoS@users.noreply.github.com>
2022-10-13 21:10:48 +0900

    Create source.cpp

---
+++ 2022-10/221012 - A형 대비 SWEA 문제 풀이/221012_스터디_황효상/[SWEA]수영장/source.cpp
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <memory.h>
+using namespace std;
+
+
+
+int T;
+int month[13];
+int cost[4];
+int dp[13][4];
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+//	freopen("input.txt", "r", stdin);
+	cin >> T;
+	for (int t = 1; t <= T; ++t) {
+		memset(month, 0, sizeof(month));
+		memset(cost, 0, sizeof(cost));
+		memset(dp, 0, sizeof(dp));
+		cin >> cost[0] >> cost[1] >> cost[2] >> cost[3];
+		for (int a = 1; a <= 12; ++a) {
+			cin >> month[a];
+		}
+      for(int b =0;b < 4; ++b){
+        if(b==0)
+          dp[1][b] = month[1]*cost[b];
+        else
+          dp[1][b] = cost[b];
+      }
+
+		for (int a = 2; a <= 12; ++a) {
+			int mini = min(dp[a - 1][0], min(dp[a - 1][1], dp[1][3]));
+			if (a >=4) mini = min(mini, dp[a - 3][2]);
+     		 for(int b =0;b < 3; ++b){
+       		 	if(b==0)
+          			dp[a][0] = mini+ month[a]*cost[b];
+        		else
+         		 	dp[a][b] = mini+cost[b];
+     			 }
+		}
+		int ans = min(dp[12][0], min(dp[12][1], min(dp[10][2], dp[1][3])));
+		cout << "#"<<t<<" "<<ans << "\n";
+	}
+	
+	return 0;
+}
